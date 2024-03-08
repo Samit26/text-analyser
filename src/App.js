870be677d79301c66a4a6efed2286d4276d74b12
Samit26 <samitkhedekar2594@gmail.com>
2024-03-08 21:07:41 +0530

    New version with more features

--- src/App.js
+++ src/App.js
@@ -1,15 +1,32 @@
 
+import { useState } from 'react';
 import './App.css';
 import Midlepage from './Midlepage';
 import Navbar from './Navbar';
+import Alert from './Alert';
 
 function App() {
+  const [alert, setAlert] = useState(null);
+
+  const showAlert= (message,type) => {
+       setAlert({ 
+        msg:message , type:type
+
+       });
+    setTimeout(() =>{
+       setAlert(null);
+
+    },1500);
+
+  }
+
   return (
     <>
 
     <Navbar />
+    <Alert alert = {alert}/>
     <div className='container'>
-    <Midlepage />
+    <Midlepage  showAlert={showAlert} />
     </div>
 
    
--- src/Midlepage.js
+++ src/Midlepage.js
@@ -3,24 +3,45 @@ import React,{useState} from 'react'
 import './Midlepage.css';
 
 
-export default function Midlepage() {
+
+export default function Midlepage(props) {
 
     const [Text, setText] = useState('Enter Text Here');
 
     const handleUpClick=() => {
         let newText = Text.toUpperCase();
         setText(newText);
+        props.showAlert("Converted to UpperCase", "success");
     }
 
     const handleLoClick=() => {
         let newText = Text.toLowerCase();
         setText(newText);
+        props.showAlert("Converted to LowerCase", "success");
     }
 
     const handleClear=() => {
         
-        setText("Enter Text Here");
+        setText("");
+        props.showAlert("Text Clear Successful", "success");
     }
+
+    const handleSpace = () => {
+      const newText = Text.replace(/\s+/g, ' ');
+      setText(newText.trim());
+      props.showAlert("Done", "success");
+  }
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(Text)
+      .then(() => {
+        props.showAlert("Successfully copied to clipboard", "success");
+      })
+      .catch(error => {
+      props.showAlert("Error copying text", "danger");
+      });
+  };
+
 
     const handleOnChange=(event) => {
          setText(event.target.value)
@@ -35,13 +56,18 @@ export default function Midlepage() {
   <button type="button" onClick={handleUpClick} className="btn btn-primary mb-3" id='Upper_btn'>Upper Case</button>
   <button type="button" onClick={handleLoClick} className="btn btn-primary mb-3" >Lower Case</button>
   <button type="button" onClick={handleClear} className="btn btn-primary mb-3" id='Clear_btn'>Clear All</button>
+  <button type="button" onClick={handleSpace} className="btn btn-primary mb-3" id='Clear_btn'>Remove Extra Space</button>
+  <button type="button" onClick={handleCopy} className="btn btn-primary mb-3" id='Clear_btn'>Copy Text</button>
 
   </div>
 
   <div className='container'>
-    <h4>Your text has {Text.split(" ").length} Words and {Text.length} Characters</h4>
+    <h4>Your text has {Text.trim() === "" ? 0 : Text.trim().split(" ").length} Words and {Text.length} Characters</h4>
   </div>
-
+   
+   <div className='container'>
+    <h4>Average time to read this text is {(Text.length/183).toFixed(2)} minutes.</h4>
+   </div>
 
    </>
   );
